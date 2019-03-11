339fa580c13eed20b4842b9330f56736c50cb8f3
Christa Stephens <ChristaStephens@outlook.com>
2019-03-10 21:59:19 -0400

    working on example from Kent Beck - to further understanding of TDD

--- src/family/FamilyPuzzleTest.java
+++ src/family/FamilyPuzzleTest.java
@@ -6,15 +6,26 @@ import org.junit.Test;
 
 public class FamilyPuzzleTest {
 	//test cases
+	//what test do we need first? -building a board, create the space/grid
 
 	//for failures
-	@Test(expected=IndexOutOfBoundsException.class)
-	public void WhenMellieLooksHorizontallySheReturnXY() {
-		FamilyPuzzle horizontal =new FamilyPuzzle();
-		//I'm expecting the coordinates 1,6, want to get the class of xy
-		assertEquals("1,6", horizontal.getClass());
+//	@Test(expected=IndexOutOfBoundsException.class)
+//	public void WhenMellieLooksHorizontallySheReturnXY() {
+//		FamilyPuzzle horizontal =new FamilyPuzzle();
+//		//I'm expecting the coordinates 1,6, want to get the class of xy
+//		assertEquals("1,6", horizontal.getClass());
+	
+	@Test
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
 		
-	}
-
+		//.times 2 is a method for multiplication
+		five.times(2);
+		//could use just equals if we know what the sum will be.
+		//assert equals gives -> (expected, actual)
+		assertEquals(10, five.amount);
+		}
+		
+	
 	
 }
